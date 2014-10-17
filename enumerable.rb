8b41bb2bdec64fb7001ace78f611182e1b94b7c4
Tomas <tebatalla@gmail.com>
2014-10-16 20:12:54 -0500

    Adding my_each, my_each_with_index and my_select

--- enumerable.rb
+++ enumerable.rb
@@ -1,7 +1,29 @@
 module Enumerable
 		
 	def my_each
-		
+		i = 0
+		while i < self.size
+			yield(self[i])
+			i += 1
+		end
 	end
 	
+	def my_each_with_index
+		i = 0
+		while i < self.size
+			yield(self[i], i)
+			i += 1
+		end
+	end
+
+	def my_select
+		select_result = []
+		self.my_each do |i|
+			if yield(i)
+				select_result << i
+			end
+		end
+		return select_result
+	end
+
 end

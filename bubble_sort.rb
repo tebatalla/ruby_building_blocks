41322468ab80bcead1fab5e4f624b662a82913a7
Tomas <tebatalla@gmail.com>
2014-10-16 19:49:24 -0500

    Completed bubble_sort

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(arr)
+	elements_left_to_sort = arr.length - 1
+	is_sorted = false
+	until is_sorted || elements_left_to_sort == 0
+		index = 0
+		is_sorted = true
+		while index < elements_left_to_sort
+			if arr[index] > arr[index+1]
+				swap = arr[index+1]
+				arr[index+1] = arr[index]
+				arr[index] = swap
+				is_sorted = false
+			end
+			index += 1
+		end
+		elements_left_to_sort -= 1
+	end
+	return arr
+end
